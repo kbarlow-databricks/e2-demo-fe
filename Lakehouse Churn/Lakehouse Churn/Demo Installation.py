7f14cbb1d8b8dc817025a495440c93bc51c067b2
kbarlow-databricks <kevin.barlow@databricks.com>
2023-08-02 16:53:50 -0700

    Added C360 Demo content

---
+++ Lakehouse Churn/Lakehouse Churn/Demo Installation.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+# MAGIC %pip install dbdemos
+
+# COMMAND ----------
+
+import dbdemos
+dbdemos.list_demos()
+
+# COMMAND ----------
+
+dbdemos.install('lakehouse-retail-c360')
